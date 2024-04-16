2c911d3555ea314dbfb03a67659c4b2b1db6f5ea
Luan de Oliveira <luan012012@hotmail.com>
2024-04-15 22:08:20 -0300

    Primeiro commit

---
+++ src/main/java/com/flex/springsecuritypractice/SpringsecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.flex.springsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flex/springsecuritypractice/SpringsecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flex.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
