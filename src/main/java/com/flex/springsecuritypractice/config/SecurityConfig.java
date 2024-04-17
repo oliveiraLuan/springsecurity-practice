357423ef6f5fff857e5c3b7d124b0b922e286a2d
Luan de Oliveira <luan012012@hotmail.com>
2024-04-16 21:56:07 -0300

    feat: adicionado defaultSecurityChain e userDetailsService

--- src/main/java/com/flex/springsecuritypractice/config/SecurityConfig.java
+++ src/main/java/com/flex/springsecuritypractice/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
@@ -34,5 +38,26 @@ public class SecurityConfig {
         http.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityChain(HttpSecurity http) throws Exception{
+        //Security Filter Chain para autenticação
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest()
+                        .authenticated())
+                        .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        //Uma instância de UserDetailsService para recuperar usuários para autenticação
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("loliveirap")
+                .password("c2XpY3VyaW9zb2Vt")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
